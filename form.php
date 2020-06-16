7853bff18ce440abf53bdddcb0fac0da24af0385
vineelavinny <rajulapati.vineela@gmail.com>
2020-06-16 20:15:10 +0530

    Update form.php

--- form.php
+++ form.php
@@ -4,11 +4,16 @@ if(mysqli_connect_errno()){
     echo 'error';
 }
 else{
-        $name=$_POST['name'];
-        $email=$_POST['email'];
-        $phonenumber=$_POST['phonenumber'];
-        $branch=$_POST['branch'];
-        $query="INSERT INTO form(name,email,mobile,branch) VALUES('$name','$email','$phonenumber','$branch')";
+        $newregister=new register;
+        $newregister->name=$_POST['name'];
+        $newregister->email=$_POST['email'];
+        $newregister->phonenumber=$_POST['phonenumber'];
+        $newregister->branch=$_POST['branch'];
+        $query="INSERT INTO form(name,email,mobile,branch) 
+                VALUES('$newregister->name',
+                       '$newregister->email',
+                       '$newregister->phonenumber',
+                       '$newregister->branch')";
         if(mysqli_query($conn,$query)){
                 header('LOCATION: index.php');
         }
